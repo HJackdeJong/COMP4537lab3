47f01ed9a5a03d11c7ab13b89acc74cf5cb6cb7e
Harrison de Jong <harrisondijon@gmail.com>
2024-09-29 19:17:53 -0700

    Testing new version

--- COMP4537/labs/3/api/server.js
+++ COMP4537/labs/3/api/server.js
@@ -1,13 +1,9 @@
-const { getDate } = require('../modules/utils');
-const locals = require('../locals/en/en.js');
-
-
 export default function handler(req, res) {
     const url = new URL(req.url, `http://${req.headers.host}`);
     const name = url.searchParams.get('name') || "Guest";
-    const currentDate = getDate();
+    const currentDate = new Date().toString();  // Assuming you just need a simple date for now
 
-    const message = locals.MESSAGES.message.replace("%1", name).concat(currentDate);
+    const message = `Hello ${name}, What a beautiful day. Server current date and time is: ${currentDate}`;
 
     res.setHeader("Content-Type", "text/html");
     res.status(200).send(`<div style="color:blue">${message}</div>`);
