0edd7975cba8552abf24fa0a71d205ceb53f019b
Harrison de Jong <harrisondijon@gmail.com>
2024-09-29 18:57:46 -0700

    rearrage for hosting

---
+++ COMP4537/labs/3/getDate/api/server.js
@@ -0,0 +1,13 @@
+const { getDate } = require('../modules/utils');
+const locals = require('../locals/en/en.js');
+
+export default function handler(req, res) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const name = url.searchParams.get('name') || "Guest";
+    const currentDate = getDate();
+
+    const message = locals.MESSAGES.message.replace("%1", name).concat(currentDate);
+
+    res.setHeader("Content-Type", "text/html");
+    res.status(200).send(`<div style="color:blue">${message}</div>`);
+}
