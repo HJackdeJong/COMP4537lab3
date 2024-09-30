bcdaf08bd25c15155163b2bb74548c32572886ac
Harrison de Jong <harrisondijon@gmail.com>
2024-09-30 11:01:21 -0700

    Added in consideration for daylight savings time

--- COMP4537/labs/3/getDate/modules/utils.js
+++ COMP4537/labs/3/getDate/modules/utils.js
@@ -1,17 +1,16 @@
 function getDate() {
-    // Get the current date and time in the Pacific Time Zone (PST/PDT)
     const now = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-
-    // Create a Date object from the converted Pacific Time string
     const pacificDate = new Date(now);
 
-    // Convert the date to a string, which will be in the format: 
-    // "Wed Sep 01 2023 12:52:14 GMT+0000 (Coordinated Universal Time)"
     let dateString = pacificDate.toString();
 
-    // Replace "+0000 (Coordinated Universal Time)" with "-0800 (Pacific Standard Time)"
-    // You may also want to handle daylight saving time (PDT) here, depending on your requirements
-    dateString = dateString.replace("GMT+0000 (Coordinated Universal Time)", "GMT-0800 (Pacific Standard Time)");
+    const isDST = pacificDate.toLocaleString('en-US', { timeZone: 'America/Los_Angeles', timeZoneName: 'short' }).includes('PDT');
+
+    if (isDST) {
+        dateString = dateString.replace("GMT+0000 (Coordinated Universal Time)", "GMT-0700 (Pacific Daylight Time)");
+    } else {
+        dateString = dateString.replace("GMT+0000 (Coordinated Universal Time)", "GMT-0800 (Pacific Standard Time)");
+    }
 
     return dateString;
 }
