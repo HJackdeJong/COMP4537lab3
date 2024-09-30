48bac4ce69e82a0dc4a5779c984df3855ed52339
Harrison de Jong <harrisondijon@gmail.com>
2024-09-30 11:32:15 -0700

    Reversion for testing

--- COMP4537/labs/3/getDate/lang/en/en.js
+++ COMP4537/labs/3/getDate/lang/en/en.js
@@ -1,15 +1,5 @@
 const MESSAGES = {
-    message: "Hello %1, What a beautiful day. Server current data and time is: ",
+    message: "Hello %1, What a beautiful day. Server current data and time is: "
+}
 
-    bucketName: 'comp4537lab3bucket',
-    fileName: 'file.txt',
-    defaultName: 'Guest',
-
-    successFileWrite: 'Successfully appended to file: %s',
-    errorFileWrite: 'Error writing to file',
-    errorFileNotFound: '404: File not found',
-    errorFileRead: 'Error reading file',
-    errorNotFound: '404: Not Found'
-};
-
-module.exports = MESSAGES;
+module.exports = {MESSAGES};
