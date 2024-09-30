6875b1559fb3de59a8ab6bddfec85027cce6deff
Harrison de Jong <harrisondijon@gmail.com>
2024-09-29 20:57:14 -0700

    Adding reading and writing to text file

--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -1,25 +1,62 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+const path = require('path');
 const { getDate } = require('./modules/utils');
 const locals = require('./lang/en/en.js');
 
 const server = http.createServer((req, res) => {
-    // Parse the request URL to extract the 'name' query parameter
-    const queryObject = url.parse(req.url, true).query;
-    const name = queryObject.name || 'Guest';
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
 
-    // Get the current server date and time
-    const serverTime = getDate();
+    if (pathname === '/COMP4537/labs/3/getDate/') {
+        // Handle the /getDate endpoint
+        const name = query.name || 'Guest';
+        const serverTime = getDate();
+        const message = locals.MESSAGES.message.replace('%1', name).concat(serverTime);
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`<p style="color: blue;">${message}</p>`);
 
-    // Replace placeholders in the greeting message
-    // const message = GREETING_MESSAGE.replace('%1', name).replace('%2', serverTime);
-    const message = locals.MESSAGES.message
-    .replace("%1", name)
-    .concat(serverTime);
+    } else if (pathname === '/COMP4537/labs/3/writeFile/') {
+        // Handle the /writeFile endpoint
+        const textToWrite = query.text || '';
+        const filePath = path.join(__dirname, 'file.txt');
+        
+        fs.appendFile(filePath, `${textToWrite}\n`, (err) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                return res.end('Error writing to file');
+            }
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(`Successfully appended to file: ${textToWrite}`);
+        });
 
-    // Set the response headers and send the styled message
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`<p style="color: blue;">${message}</p>`);
+    } else if (pathname === '/COMP4537/labs/3/readFile/file.txt') {
+        // Handle the /readFile endpoint
+        const filePath = path.join(__dirname, 'file.txt');
+
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('404: File not found');
+                } else {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Error reading file');
+                }
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(data);
+        });
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404: Not Found');
+    }
 });
 
 // Conditional for local testing
