ed785db38557dd85d1da4ddc97d56c9733bbe3b4
Harrison de Jong <harrisondijon@gmail.com>
2024-09-30 11:32:19 -0700

    Reversion for testing #2

--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -2,11 +2,12 @@ const http = require('http');
 const url = require('url');
 const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { getDate } = require('./modules/utils');
-const MESSAGES = require('./lang/en/en.js'); 
+const locals = require('./lang/en/en.js');
 require('dotenv').config();
 
+// Configure the S3 Client using environment variables
 const s3 = new S3Client({
-    region: 'us-east-2',
+    region: 'us-east-2', // Ensure this is the correct region for your bucket
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -14,32 +15,35 @@ const s3 = new S3Client({
     endpoint: `https://s3.us-east-2.amazonaws.com`,
 });
 
-const bucketName = MESSAGES.bucketName;
-const fileName = MESSAGES.fileName;
+const bucketName = 'comp4537lab3bucket';
+const fileName = 'file.txt';
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
     const query = parsedUrl.query;
 
-    if (pathname === MESSAGES.paths.getDate) { 
-        const name = query.name || MESSAGES.defaultName;
+    if (pathname === '/COMP4537/labs/3/getDate/') {
+        const name = query.name || 'Guest';
         const serverTime = getDate();
-        const message = MESSAGES.message.replace('%1', name).concat(serverTime);
+        const message = locals.MESSAGES.message.replace('%1', name).concat(serverTime);
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(`<p style="color: blue;">${message}</p>`);
 
-    } else if (pathname === MESSAGES.paths.writeFile) {  
+    } else if (pathname === '/COMP4537/labs/3/writeFile/') {
         const textToWrite = query.text || '';
 
+        // Read the file from S3
         s3.send(new GetObjectCommand({ Bucket: bucketName, Key: fileName }))
             .then(data => {
+                // Convert the Body stream to a string
                 return streamToString(data.Body);
             })
             .then(currentContent => {
                 const updatedContent = currentContent + `\n${textToWrite}`;
 
+                // Write the updated content back to S3
                 return s3.send(new PutObjectCommand({
                     Bucket: bucketName,
                     Key: fileName,
@@ -49,14 +53,15 @@ const server = http.createServer((req, res) => {
             })
             .then(() => {
                 res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(MESSAGES.successFileWrite.replace('%s', textToWrite));
+                res.end(`Successfully appended to file: ${textToWrite}`);
             })
             .catch(err => {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end(MESSAGES.errorFileWrite);
+                res.end('Error writing to file');
             });
 
-    } else if (pathname === MESSAGES.paths.readFile) {  // Use MESSAGES for paths
+    } else if (pathname === '/COMP4537/labs/3/readFile/file.txt') {
+        // Read the file from S3
         s3.send(new GetObjectCommand({ Bucket: bucketName, Key: fileName }))
             .then(data => {
                 return streamToString(data.Body);
@@ -68,19 +73,20 @@ const server = http.createServer((req, res) => {
             .catch(err => {
                 if (err.name === 'NoSuchKey') {
                     res.writeHead(404, { 'Content-Type': 'text/plain' });
-                    res.end(MESSAGES.errorFileNotFound);
+                    res.end('404: File not found');
                 } else {
                     res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end(MESSAGES.errorFileRead);
+                    res.end('Error reading file');
                 }
             });
 
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(MESSAGES.errorNotFound);
+        res.end('404: Not Found');
     }
 });
 
+// Utility function to convert a stream to a string
 function streamToString(stream) {
     return new Promise((resolve, reject) => {
         const chunks = [];
@@ -90,5 +96,9 @@ function streamToString(stream) {
     });
 }
 
-
-module.exports = server;
+// Conditional for local testing
+if (require.main === module) {
+    const PORT = 3000;
+} else {
+    module.exports = server;
+}
